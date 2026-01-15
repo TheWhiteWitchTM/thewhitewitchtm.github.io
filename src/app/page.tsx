6cb960409d8acf68ba342d1c070868038674de0b
TheWhiteWitchTM <TheWhiteWitchTM@gmail.com>
2026-01-15 13:57:47 +0100

    Logo Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import { FaMoon, FaRegMoon } from 'react-icons/fa';
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
+        <Image
+          src="/logo.jpg"
+          alt="TheWhiteWitchTM Logo"
+          width={128}   
+          height={128}
+        />
+
         🧙‍♀️𝕿𝖍𝖊 𝖂𝖍𝖎𝖙𝖊 𝖂𝖎𝖙𝖈𝖍™✨
         <br/>
         @TheWhiteWitchTM
         <p>
-          <FaMoon />Something, something Blog! Eventually...
+          Something, something Blog! Eventually...
         </p>
       </main>
     </div>
